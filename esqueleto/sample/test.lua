b22c54a8d2ff1ec73e13b36de9f18a39f8038bdc
gonzalomvp <gonzalomvp@hotmail.com>
2018-05-06 22:25:34 +0200

    Added ghost in LUA to implement pursue

--- esqueleto/sample/test.lua
+++ esqueleto/sample/test.lua
@@ -28,6 +28,25 @@ entity:setRot(-135)
 --entity:setLinearVel(10, 20)
 --entity:setAngularVel(30)
 
+
+texture_name = "ghost32.png"
+gfxQuad = MOAIGfxQuad2D.new()
+gfxQuad:setTexture(texture_name)
+char_size = 32
+gfxQuad:setRect(-char_size/2, -char_size/2, char_size/2, char_size/2)
+gfxQuad:setUVRect(0, 0, 1, 1)
+  
+prop = MOAIProp2D.new()
+prop:setDeck(gfxQuad)
+
+entity2 = Character.new()
+-- Add prop to be the renderable for this character
+entity2:setProp(prop, layer)
+-- Start the character (allow calls to OnUpdate)
+entity2:start()
+entity2:setLoc(100, 100)
+entity2:setRot(90)
+
 -- Enable Debug Draw
 debug = MOAIDrawDebug.get();
 layer:setDrawDebug(debug)
@@ -35,10 +54,10 @@ layer:setDrawDebug(debug)
 MOAIDrawDebug.insertEntity(entity)
 
 
-pathfinder = Pathfinder.new()
-pathfinder:setStartPosition(5, 10)
-pathfinder:setEndPosition(20, 40)
-MOAIDrawDebug.insertEntity(pathfinder)
+--pathfinder = Pathfinder.new()
+--pathfinder:setStartPosition(5, 10)
+--pathfinder:setEndPosition(20, 40)
+--MOAIDrawDebug.insertEntity(pathfinder)
 
 mouseX = 0
 mouseY = 0
